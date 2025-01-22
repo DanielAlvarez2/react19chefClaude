d4a50fbd0c0629a33a0d3b19f19a5236a862707c
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-01-22 01:53:18 -0500

    fcc section 3 lesson 38 chef claude conditional rendering challenge 1 / completed

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -23,10 +23,17 @@ export default function Main(){
                     type="submit" 
                     value="+ Add ingredient"/>
             </form>
-
-            <ul>
-                {list}
-            </ul>
+            {ingredients.length > 0 && <section>
+                <h2>Ingredients on hand:</h2>
+                <ul>{list}</ul>
+                <div className='get-recipe-container'>
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>
+            </section>}
         </>
     )
 }
